d5b383313c414392e5eb8653a808e0a145f1e716
Claudio Carigé <ccarige@gmail.com>
2024-04-16 19:54:49 -0300

    [addition] : First cimmit

---
+++ src/test/java/br/com/copyimagem/CopyimagemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.copyimagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyimagemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
