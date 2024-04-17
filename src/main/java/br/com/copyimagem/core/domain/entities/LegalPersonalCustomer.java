f20213e6457d4fd073ef64db5ad66fbb6de5a7e7
Claudio Carigé <ccarige@gmail.com>
2024-04-17 14:09:42 -0300

    [addition] : Created LegalPersonalCustomer class

---
+++ src/main/java/br/com/copyimagem/core/domain/entities/LegalPersonalCustomer.java
@@ -0,0 +1,29 @@
+package br.com.copyimagem.core.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.io.Serial;
+
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class LegalPersonalCustomer extends Customer{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(nullable = false, unique = true,length = 18)
+    private String cnpj;
+
+    @Override
+    public int hashCode() { return super.hashCode(); }
+
+    @Override
+    public boolean equals(Object obj) { return super.equals(obj); }
+}
