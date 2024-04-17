8de08d75f7ab4c8d5628440e4647e6a2a32405ba
Claudio Carigé <ccarige@gmail.com>
2024-04-17 14:11:58 -0300

    [addition] : Created MultiPrinter Class

---
+++ src/main/java/br/com/copyimagem/core/domain/entities/MultiPrinter.java
@@ -0,0 +1,40 @@
+package br.com.copyimagem.core.domain.entities;
+
+import br.com.copyimagem.core.domain.enums.MachineStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MultiPrinter implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private String brand;
+    private String model;
+    @Column(unique = true)
+    private String serialNumber;
+    private Double machineValue;
+    @Enumerated(EnumType.STRING)
+    private MachineStatus machineStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+}
