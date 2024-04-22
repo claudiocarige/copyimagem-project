f33d1d25d5cb6b9aeac58360b807653cd125ebdc
Claudio Carigé <ccarige@gmail.com>
2024-04-21 22:42:09 -0300

    [addition] : Created MultiPrinterBuilder class for tests

---
+++ src/test/java/br/com/copyimagem/core/domain/builders/MultiPrinterBuilder.java
@@ -0,0 +1,72 @@
+package br.com.copyimagem.core.domain.builders;
+
+import br.com.copyimagem.core.domain.entities.Customer;
+import br.com.copyimagem.core.domain.enums.MachineStatus;
+import br.com.copyimagem.core.domain.entities.MultiPrinter;
+
+public class MultiPrinterBuilder {
+    private Integer id;
+    private String brand;
+    private String model;
+    private String serialNumber;
+    private Double machineValue;
+    private MachineStatus machineStatus;
+    private Customer customer;
+
+    private MultiPrinterBuilder(){}
+
+    public static MultiPrinterBuilder oneMultiPrinter() {
+        MultiPrinterBuilder builder = new MultiPrinterBuilder();
+        initializeDefaultData(builder);
+        return builder;
+    }
+
+    private static void initializeDefaultData(MultiPrinterBuilder builder) {
+        builder.id = 1;
+        builder.brand = "Epson";
+        builder.model = "L5290";
+        builder.serialNumber = "x1x2x3";
+        builder.machineValue = 1000.0;
+        builder.machineStatus = MachineStatus.DISPONIVEL;
+    }
+
+
+    public MultiPrinterBuilder withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public MultiPrinterBuilder withBrand(String brand) {
+        this.brand = brand;
+        return this;
+    }
+
+    public MultiPrinterBuilder withModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public MultiPrinterBuilder withSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+        return this;
+    }
+
+    public MultiPrinterBuilder withMachineValue(Double machineValue) {
+        this.machineValue = machineValue;
+        return this;
+    }
+
+    public MultiPrinterBuilder withMachineStatus(MachineStatus machineStatus) {
+        this.machineStatus = machineStatus;
+        return this;
+    }
+
+    public MultiPrinterBuilder withCustomer(Customer customer) {
+        this.customer = customer;
+        return this;
+    }
+
+    public MultiPrinter now() {
+        return new MultiPrinter(id, brand, model, serialNumber, machineValue, machineStatus, customer);
+    }
+}
