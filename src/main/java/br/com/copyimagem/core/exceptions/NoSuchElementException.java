d0866b483e3ec818656bb33a8ec5a2c8a9b56534
Claudio Carigé <ccarige@gmail.com>
2024-04-22 19:20:59 -0300

    [addition] : Created NoSuchElementException class

---
+++ src/main/java/br/com/copyimagem/core/exceptions/NoSuchElementException.java
@@ -0,0 +1,7 @@
+package br.com.copyimagem.core.exceptions;
+
+public class NoSuchElementException extends RuntimeException{
+    public NoSuchElementException(String message) {
+        super(message);
+    }
+}
