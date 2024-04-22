0404842ac8fda01a5ce35ea40659a206a2c8c6e9
Claudio Carigé <ccarige@gmail.com>
2024-04-22 19:19:46 -0300

    [addition] : Created NaturalPersonCustomerServiceImpl class

---
+++ src/main/java/br/com/copyimagem/core/usecases/impl/NaturalPersonCustomerServiceImpl.java
@@ -0,0 +1,34 @@
+package br.com.copyimagem.core.usecases.impl;
+
+import br.com.copyimagem.core.domain.entities.NaturalPersonCustomer;
+import br.com.copyimagem.core.usecases.interfaces.NaturalPersonCustomerService;
+import br.com.copyimagem.infra.repositories.NaturalPersonCustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NaturalPersonCustomerServiceImpl implements NaturalPersonCustomerService {
+
+    private final NaturalPersonCustomerRepository naturalPersonCustomerRepository;
+
+    public NaturalPersonCustomerServiceImpl(NaturalPersonCustomerRepository naturalPersonCustomerRepository) {
+        this.naturalPersonCustomerRepository = naturalPersonCustomerRepository;
+    }
+
+    @Override
+    public NaturalPersonCustomer findNaturalPersonCustomerById(Long id) {
+        return naturalPersonCustomerRepository.findById(id).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+    @Override
+    public List<NaturalPersonCustomer> findAllNaturalPersonCustomer() {
+        return naturalPersonCustomerRepository.findAll();
+    }
+
+    @Override
+    public NaturalPersonCustomer saveNaturalPersonCustomer(NaturalPersonCustomer naturalPersonCustomer) {
+        return naturalPersonCustomerRepository.save(naturalPersonCustomer);
+    }
+}
