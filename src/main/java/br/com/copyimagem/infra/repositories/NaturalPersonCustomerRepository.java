d4e73732a997e65fdb2398e393a2eb9876d60092
Claudio Carigé <ccarige@gmail.com>
2024-04-22 21:30:59 -0300

    [addition] : Created findByPrimaryEmail method

--- src/main/java/br/com/copyimagem/infra/repositories/NaturalPersonCustomerRepository.java
+++ src/main/java/br/com/copyimagem/infra/repositories/NaturalPersonCustomerRepository.java
@@ -3,5 +3,8 @@ package br.com.copyimagem.infra.repositories;
 import br.com.copyimagem.core.domain.entities.NaturalPersonCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NaturalPersonCustomerRepository extends JpaRepository<NaturalPersonCustomer, Long>{
+    Optional<NaturalPersonCustomer> findByPrimaryEmail(String primaryEmail);
 }
