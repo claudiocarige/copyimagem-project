188c3393f729a4bd34fb31e5ba843db982f47f68
Claudio Carigé <ccarige@gmail.com>
2024-04-24 15:49:04 -0300

    [addition] : Created NaturalPersonCustomerController class

---
+++ src/main/java/br/com/copyimagem/core/controllers/NaturalPersonCustomerController.java
@@ -0,0 +1,25 @@
+package br.com.copyimagem.core.controllers;
+
+import br.com.copyimagem.core.domain.entities.NaturalPersonCustomer;
+import br.com.copyimagem.core.usecases.interfaces.NaturalPersonCustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers/pf")
+public class NaturalPersonCustomerController {
+
+    private final NaturalPersonCustomerService naturalPersonCustomerService;
+
+    public NaturalPersonCustomerController(NaturalPersonCustomerService naturalPersonCustomerService) {
+        this.naturalPersonCustomerService = naturalPersonCustomerService;
+    }
+
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<NaturalPersonCustomer>> getAllNaturalPersonCustomers(){
+        return  ResponseEntity.ok().body(naturalPersonCustomerService.findAllNaturalPersonCustomer());
+    }
+}
