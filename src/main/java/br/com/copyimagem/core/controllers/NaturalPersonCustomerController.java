80a1d8369aea18226c8bf6f3f15bcd8e4538448e
Claudio Carigé <ccarige@gmail.com>
2024-04-26 11:14:41 -0300

    [addition] : Created saveNaturalPersonCustomer method in NaturalPersonCustomerController

--- src/main/java/br/com/copyimagem/core/controllers/NaturalPersonCustomerController.java
+++ src/main/java/br/com/copyimagem/core/controllers/NaturalPersonCustomerController.java
@@ -2,9 +2,12 @@ package br.com.copyimagem.core.controllers;
 
 import br.com.copyimagem.core.domain.entities.NaturalPersonCustomer;
 import br.com.copyimagem.core.usecases.interfaces.NaturalPersonCustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,6 +29,16 @@ public class NaturalPersonCustomerController {
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<NaturalPersonCustomer> getNaturalPersonCustomerById(@PathVariable Long id){
         return ResponseEntity.ok().body(naturalPersonCustomerService.findNaturalPersonCustomerById(id));
+    }
+
+    @PostMapping(value = "/save", produces = "application/json")
+    public ResponseEntity<HttpStatus> saveNaturalPersonCustomer(
+            @RequestBody NaturalPersonCustomer naturalPersonCustomer){
+        NaturalPersonCustomer naturalPersonCustomer1 = naturalPersonCustomerService.saveNaturalPersonCustomer(naturalPersonCustomer);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(naturalPersonCustomer1.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
