48e8f54e2a57682419870cb0eba77c9a44c446a2
Claudio Carigé <ccarige@gmail.com>
2024-04-27 10:31:34 -0300

    [addition] : Created the ConvertObjectToObjectDTOService class

---
+++ src/main/java/br/com/copyimagem/core/usecases/impl/ConvertObjectToObjectDTOService.java
@@ -0,0 +1,25 @@
+package br.com.copyimagem.core.usecases.impl;
+
+import br.com.copyimagem.core.domain.entities.NaturalPersonCustomer;
+import br.com.copyimagem.core.dtos.NaturalPersonCustomerDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConvertObjectToObjectDTOService {
+
+    private ModelMapper modelMapper;
+
+    public ConvertObjectToObjectDTOService(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public NaturalPersonCustomerDTO convertToNaturalPersonCustomerDTO(NaturalPersonCustomer naturalPersonCustomer) {
+        return modelMapper.map(naturalPersonCustomer, NaturalPersonCustomerDTO.class);
+    }
+
+    public NaturalPersonCustomer convertToNaturalPersonCustomer(NaturalPersonCustomerDTO naturalPersonCustomerDTO) {
+        return modelMapper.map(naturalPersonCustomerDTO, NaturalPersonCustomer.class);
+    }
+
+}
