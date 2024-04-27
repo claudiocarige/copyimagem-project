9d1e1aa833fa4a34de85fd3bb9bb14c36f3d2003
Claudio Carigé <ccarige@gmail.com>
2024-04-27 20:26:04 -0300

    [test refactor] : Renamed from CustomerBuilder  to LegalPersonalCustomerBuilder

--- src/test/java/br/com/copyimagem/core/domain/builders/LegalPersonalCustomerBuilder.java
+++ src/test/java/br/com/copyimagem/core/domain/builders/LegalPersonalCustomerBuilder.java
@@ -1,17 +1,22 @@
 package br.com.copyimagem.core.domain.builders;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
 
 import br.com.copyimagem.core.domain.entities.*;
 import br.com.copyimagem.core.domain.enums.FinancialSituation;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static br.com.copyimagem.core.domain.builders.AdressBuilder.oneAdress;
 import static br.com.copyimagem.core.domain.builders.MonthlyPaymentBuilder.oneMonthlyPayment;
 import static br.com.copyimagem.core.domain.builders.MultiPrinterBuilder.oneMultiPrinter;
 
-public class CustomerBuilder {
+public class LegalPersonalCustomerBuilder implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String clientName;
     private String primaryEmail;
@@ -25,18 +30,17 @@ public class CustomerBuilder {
     private byte payDay;
     private List<MultiPrinter> multiPrinterList = new ArrayList<>();
     private List<MonthlyPayment> monthlyPaymentList = new ArrayList<>();
-    private String cpf;
     private String cnpj;
 
-    private CustomerBuilder(){}
+    private LegalPersonalCustomerBuilder(){}
 
-    public static CustomerBuilder oneCustomer() {
-        CustomerBuilder builder = new CustomerBuilder();
+    public static LegalPersonalCustomerBuilder oneCustomer() {
+        LegalPersonalCustomerBuilder builder = new LegalPersonalCustomerBuilder();
         initializeDefaultData(builder);
         return builder;
     }
 
-    private static void initializeDefaultData(CustomerBuilder builder) {
+    private static void initializeDefaultData(LegalPersonalCustomerBuilder builder) {
         builder.id = 1L;
         builder.clientName = "Claudio Carigé";
         builder.primaryEmail = "carige@mail.com";
@@ -52,93 +56,78 @@ public class CustomerBuilder {
         builder.monthlyPaymentList = Arrays.asList(oneMonthlyPayment().now());
     }
 
-    public CustomerBuilder withId(Long id) {
+    public LegalPersonalCustomerBuilder withId(Long id) {
         this.id = id;
         return this;
     }
 
-    public CustomerBuilder withClientName(String clientName) {
+    public LegalPersonalCustomerBuilder withClientName(String clientName) {
         this.clientName = clientName;
         return this;
     }
 
-    public CustomerBuilder withPrimaryEmail(String primaryEmail) {
+    public LegalPersonalCustomerBuilder withPrimaryEmail(String primaryEmail) {
         this.primaryEmail = primaryEmail;
         return this;
     }
 
-    public CustomerBuilder withListaEmailList(String... emailList) {
+    public LegalPersonalCustomerBuilder withListaEmailList(String... emailList) {
         this.emailList = Arrays.asList(emailList);
         return this;
     }
 
-    public CustomerBuilder withPhoneNumber(String phoneNumber) {
+    public LegalPersonalCustomerBuilder withPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
         return this;
     }
 
-    public CustomerBuilder withWhatsapp(String whatsapp) {
+    public LegalPersonalCustomerBuilder withWhatsapp(String whatsapp) {
         this.whatsapp = whatsapp;
         return this;
     }
 
-    public CustomerBuilder withBankCode(String bankCode) {
+    public LegalPersonalCustomerBuilder withBankCode(String bankCode) {
         this.bankCode = bankCode;
         return this;
     }
 
-    public CustomerBuilder withAdress(Adress adress) {
+    public LegalPersonalCustomerBuilder withAdress(Adress adress) {
         this.adress = adress;
         return this;
     }
 
-    public CustomerBuilder withStartContract(LocalDate startContract) {
+    public LegalPersonalCustomerBuilder withStartContract(LocalDate startContract) {
         this.startContract = startContract;
         return this;
     }
 
-    public CustomerBuilder withFinancialSituation(FinancialSituation financialSituation) {
+    public LegalPersonalCustomerBuilder withFinancialSituation(FinancialSituation financialSituation) {
         this.financialSituation = financialSituation;
         return this;
     }
 
-    public CustomerBuilder withPayDay(byte payDay) {
+    public LegalPersonalCustomerBuilder withPayDay(byte payDay) {
         this.payDay = payDay;
         return this;
     }
 
-    public CustomerBuilder withListaMultiPrinterList(MultiPrinter... multiPrinterList) {
+    public LegalPersonalCustomerBuilder withListaMultiPrinterList(MultiPrinter... multiPrinterList) {
         this.multiPrinterList = Arrays.asList(multiPrinterList);
         return this;
     }
 
-    public CustomerBuilder withListaMonthlyPaymentList(MonthlyPayment... monthlyPaymentList) {
+    public LegalPersonalCustomerBuilder withListaMonthlyPaymentList(MonthlyPayment... monthlyPaymentList) {
         this.monthlyPaymentList = Arrays.asList(monthlyPaymentList);
         return this;
     }
 
-    public CustomerBuilder withCpf(String cpf) {
-        this.cpf = cpf;
-        return this;
-    }
-
-    public CustomerBuilder withCnpj(String cnpj) {
+    public LegalPersonalCustomerBuilder withCnpj(String cnpj) {
         this.cnpj = cnpj;
         return this;
     }
 
-    public NaturalPersonCustomer nowCustomerPF() {
-        NaturalPersonCustomer customer = new NaturalPersonCustomer(cpf);
-        now(customer);
-        return customer;
-    }
-
     public LegalPersonalCustomer nowCustomerPJ() {
         LegalPersonalCustomer customer = new LegalPersonalCustomer(cnpj);
-        now(customer);
-        return customer;    }
-
-    private void now(Customer customer) {
         customer.setId(id);
         customer.setClientName(clientName);
         customer.setPrimaryEmail(primaryEmail);
@@ -154,5 +143,6 @@ public class CustomerBuilder {
         customer.addMultiPrinter(multiPrinterList);
         monthlyPaymentList.forEach(monthlyPayment -> monthlyPayment.setCustomer(customer));
         customer.addMonthlyPayment(monthlyPaymentList);
+        return customer;
     }
 }
