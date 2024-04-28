1bf7cbd6fe2b3d72e15be6885d3a0d1598450c98
Claudio Carigé <ccarige@gmail.com>
2024-04-28 15:17:21 -0300

    [addition] : Created LegalPersonalCustomerService Interface

---
+++ src/main/java/br/com/copyimagem/core/usecases/interfaces/LegalPersonalCustomerService.java
@@ -0,0 +1,12 @@
+package br.com.copyimagem.core.usecases.interfaces;
+
+import br.com.copyimagem.core.dtos.LegalPersonalCustomerDTO;
+
+import java.util.List;
+
+public interface LegalPersonalCustomerService {
+
+    List<LegalPersonalCustomerDTO> findAllLegalPersonalCustomer();
+    LegalPersonalCustomerDTO findLegalPersonalCustomerById(Long id);
+    LegalPersonalCustomerDTO saveLegalPersonalCustomer(LegalPersonalCustomerDTO legalPersonalCustomer);
+}
