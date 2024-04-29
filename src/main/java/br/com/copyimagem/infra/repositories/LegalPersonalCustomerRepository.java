b409fcb19fcfc88dcb72ae2ec0f929922fd69a12
Claudio Carigé <ccarige@gmail.com>
2024-04-29 01:10:47 -0300

    [addition] : Created findByPrimaryEmail and findByCnpj methods

--- src/main/java/br/com/copyimagem/infra/repositories/LegalPersonalCustomerRepository.java
+++ src/main/java/br/com/copyimagem/infra/repositories/LegalPersonalCustomerRepository.java
@@ -3,5 +3,10 @@ package br.com.copyimagem.infra.repositories;
 import br.com.copyimagem.core.domain.entities.LegalPersonalCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LegalPersonalCustomerRepository extends JpaRepository<LegalPersonalCustomer, Long>{
+    Optional<LegalPersonalCustomer> findByPrimaryEmail(String primaryEmail);
+
+    Optional<LegalPersonalCustomer> findByCnpj(String cpf);
 }
