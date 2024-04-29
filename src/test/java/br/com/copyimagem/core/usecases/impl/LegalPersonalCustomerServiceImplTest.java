3a5b4135ab8fbec3528c3361277db4c2a450acb2
Claudio Carigé <ccarige@gmail.com>
2024-04-28 22:31:30 -0300

    [test] : Created shouldReturnAListfLegalPersonalCustomer  test method

--- src/test/java/br/com/copyimagem/core/usecases/impl/LegalPersonalCustomerServiceImplTest.java
+++ src/test/java/br/com/copyimagem/core/usecases/impl/LegalPersonalCustomerServiceImplTest.java
@@ -1,7 +1,10 @@
 package br.com.copyimagem.core.usecases.impl;
 
 import br.com.copyimagem.core.domain.entities.LegalPersonalCustomer;
+import br.com.copyimagem.core.domain.entities.MonthlyPayment;
+import br.com.copyimagem.core.domain.entities.MultiPrinter;
 import br.com.copyimagem.core.dtos.LegalPersonalCustomerDTO;
+import br.com.copyimagem.core.exceptions.NoSuchElementException;
 import br.com.copyimagem.infra.repositories.LegalPersonalCustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
 import java.util.Optional;
 
 import static br.com.copyimagem.core.domain.builders.LegalPersonalCustomerBuilder.oneCustomer;
@@ -43,7 +47,7 @@ class LegalPersonalCustomerServiceImplTest {
 
     @Test
     @DisplayName("Must return a LegalPersonalCustomerDTO by Id")
-    void youMustReturnALegalPersonalCustomerDTOById(){
+    void shoulReturnALegalPersonalCustomerDTOById(){
         when(legalPersonalCustomerRepository.findById(ID1L)).thenReturn(Optional.of(customerPj));
         when(convertObjectToObjectDTOService.convertToLegalPersonalCustomerDTO(customerPj)).thenReturn(customerPjDTO);
 
@@ -56,6 +60,46 @@ class LegalPersonalCustomerServiceImplTest {
                 () -> assertEquals(LegalPersonalCustomerDTO.class, legalPersonalCustomerDTO.getClass())
                 );
     }
+
+    @Test
+    @DisplayName("shoul return a empty when LegalPersonalCustomer not found")
+    void shoulReturnEmptyWhenNaturalPersonCustomerNotFound() {
+        when(legalPersonalCustomerRepository.findById(ID1L)).thenReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class,
+                () -> legalPersonalCustomerService.findLegalPersonalCustomerById(ID1L)).getMessage();
+        try {
+            legalPersonalCustomerService.findLegalPersonalCustomerById(ID1L);
+        } catch (NoSuchElementException message) {
+            assertEquals("Customer not found", message.getMessage());
+            assertEquals(NoSuchElementException.class, message.getClass());
+        }
+    }
+    @Test
+    @DisplayName("should return a list of legalPersonalCustomer")
+    void shouldReturnAListfLegalPersonalCustomer(){
+        when(legalPersonalCustomerRepository.findAll()).thenReturn(List.of(customerPj));
+        when(convertObjectToObjectDTOService.convertToLegalPersonalCustomerDTO(customerPj)).thenReturn(customerPjDTO);
+
+        List<LegalPersonalCustomerDTO> legalPersonalCustomerList = legalPersonalCustomerService.findAllLegalPersonalCustomer();
+        assertAll("LegalPersonalCustomerLis",
+                () -> assertNotNull(legalPersonalCustomerList),
+                () -> assertEquals(1 , legalPersonalCustomerList.size()),
+                () -> assertEquals(LegalPersonalCustomerDTO.class,legalPersonalCustomerList.get(0).getClass()),
+                () -> assertAll("MultPrint",
+                        () -> assertNotNull(legalPersonalCustomerList.get(0).getMultiPrinterList()),
+                        () -> assertEquals(MultiPrinter.class, legalPersonalCustomerList.get(0).getMultiPrinterList().get(0).getClass()),
+                        () -> assertEquals(1, legalPersonalCustomerList.get(0).getMultiPrinterList().size())
+                        ),
+                () -> assertAll("MonthlyPayment",
+                        () -> assertEquals(1 , legalPersonalCustomerList.get(0).getMonthlyPaymentList().size()),
+                        () -> assertEquals(MonthlyPayment.class, legalPersonalCustomerList.get(0).getMonthlyPaymentList().get(0).getClass())
+                        )
+                );
+    }
+
+
+
+
 
     private void start() {
         customerPj = oneCustomer().withId(ID1L).withCnpj(CNPJ).withPrimaryEmail("carige@mail.com").nowCustomerPJ();
