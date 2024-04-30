63a865254f2ee72bbc73e6c8d9d3b13425656ab6
Claudio Carigé <ccarige@gmail.com>
2024-04-30 14:23:12 -0300

    [addition] : Created CustomerResponseDTO class

---
+++ src/main/java/br/com/copyimagem/core/dtos/CustomerResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.copyimagem.core.dtos;
+
+import br.com.copyimagem.core.domain.entities.Adress;
+import lombok.Data;
+
+@Data
+public class CustomerResponseDTO {
+
+    private Long id;
+    private String clientName;
+    private String primaryEmail;
+    private String phoneNumber;
+    private Adress address;
+    private String cpfOrCnpj;
+
+}
