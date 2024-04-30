b321f54ed79c46f5be6de82bd0827d3296382025
Claudio Carigé <ccarige@gmail.com>
2024-04-30 13:53:09 -0300

    [addition] : Adding settings to convert CustomerResponseDTO

--- src/main/java/br/com/copyimagem/infra/config/ModelMapperConfig.java
+++ src/main/java/br/com/copyimagem/infra/config/ModelMapperConfig.java
@@ -1,6 +1,10 @@
 package br.com.copyimagem.infra.config;
 
+import br.com.copyimagem.core.domain.entities.LegalPersonalCustomer;
+import br.com.copyimagem.core.domain.entities.NaturalPersonCustomer;
+import br.com.copyimagem.core.dtos.CustomerResponseDTO;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +16,21 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        modelMapper.addMappings(new PropertyMap<LegalPersonalCustomer, CustomerResponseDTO>() {
+            @Override
+            protected void configure(){
+                map().setCpfOrCnpj(source.getCnpj());
+                map().setAddress(source.getAdress());
+            }
+        });
+        modelMapper.addMappings(new PropertyMap<NaturalPersonCustomer, CustomerResponseDTO>() {
+            @Override
+            protected void configure() {
+                map().setCpfOrCnpj(source.getCpf());
+                map().setAddress(source.getAdress());
+            }
+        });
         return modelMapper;
     }
 }
