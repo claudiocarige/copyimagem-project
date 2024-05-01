9beab9dc90f39ea9a9d7ec4e61ab0df907b2f28d
Claudio Carigé <ccarige@gmail.com>
2024-05-01 08:57:37 -0300

    [addition] : Inserted a findByPrimaryEmail method

--- src/main/java/br/com/copyimagem/infra/repositories/CustomerRepository.java
+++ src/main/java/br/com/copyimagem/infra/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package br.com.copyimagem.infra.repositories;
 import br.com.copyimagem.core.domain.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByPrimaryEmail(String email);
 }
