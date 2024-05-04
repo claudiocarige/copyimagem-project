217e6167be589ef24101526ed00b621653e49eb5
Claudio Carigé <ccarige@gmail.com>
2024-05-04 12:36:34 -0300

    [ addition ] : Created findAllByFinancialSituation query method

--- src/main/java/br/com/copyimagem/infra/persistence/repositories/CustomerRepository.java
+++ src/main/java/br/com/copyimagem/infra/persistence/repositories/CustomerRepository.java
@@ -1,10 +1,14 @@
-package br.com.copyimagem.infra.repositories;
+package br.com.copyimagem.infra.persistence.repositories;
 
 import br.com.copyimagem.core.domain.entities.Customer;
+import br.com.copyimagem.core.domain.enums.FinancialSituation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByPrimaryEmail(String email);
+
+    List<Customer> findAllByFinancialSituation(FinancialSituation financialSituation);
 }
