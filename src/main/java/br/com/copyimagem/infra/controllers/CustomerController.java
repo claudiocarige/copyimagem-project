599903b707d09565ac7c0a246acda8c2a4b22524
Claudio Carigé <ccarige@gmail.com>
2024-05-04 17:13:58 -0300

    [ feat ] : Created searchFinancialSituation method

--- src/main/java/br/com/copyimagem/infra/controllers/CustomerController.java
+++ src/main/java/br/com/copyimagem/infra/controllers/CustomerController.java
@@ -1,15 +1,13 @@
 package br.com.copyimagem.infra.controllers;
 
 import br.com.copyimagem.core.dtos.CustomerResponseDTO;
+import br.com.copyimagem.core.exceptions.IllegalArgumentException;
 import br.com.copyimagem.core.exceptions.NoSuchElementException;
 import br.com.copyimagem.core.usecases.interfaces.CustomerService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,7 +42,12 @@ public class CustomerController {
         return ResponseEntity.ok().body(customerService.searchClientAll());
     }
 
-    //TODO Create search for all Customers by FinancialSituation
-    //TODO Create search for all defaulting Customers(Create Record Object for this)
-
+    @GetMapping(value="/search-financial-situation")
+    public ResponseEntity<List<CustomerResponseDTO>> searchFinancialSituation(@RequestParam("situation") String situation) {
+        if (!situation.equals("PAGO") && !situation.equals("PENDENTE") && !situation.equals("INADIMPLENTE") && !situation.equals("CANCELADO")){
+            throw new IllegalArgumentException("The argument is not correct");
+        }
+        log.info(String.format("[ INFO ] Search for all defaulting customers --- { %s }", CustomerController.class));
+        return ResponseEntity.ok().body(customerService.searchFinancialSituation(situation));
+    }
 }
