f0714ae58a34550f7851def40dd158291f63fbf3
Claudio Carigé <ccarige@gmail.com>
2024-05-07 20:43:20 -0300

    [ test refactor ] :  Changed the name of the onsCustomer() method

--- src/test/java/br/com/copyimagem/core/domain/entities/CustomerTest.java
+++ src/test/java/br/com/copyimagem/core/domain/entities/CustomerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static br.com.copyimagem.core.domain.builders.LegalPersonalCustomerBuilder.oneCustomer;
+import static br.com.copyimagem.core.domain.builders.LegalPersonalCustomerBuilder.oneLegalPersonalCustomer;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
@@ -16,7 +16,7 @@ class CustomerTest {
     @DisplayName("You must create customer natural person customer")
     void youMustCreateCustomerNaturalPersonCustomer() {
         NaturalPersonCustomer customer =
-                br.com.copyimagem.core.domain.builders.NaturalPersonCustomerBuilder.oneCustomer()
+                br.com.copyimagem.core.domain.builders.NaturalPersonCustomerBuilder.oneNaturalPersonCustomer()
                         .withCpf("123.456.789.10").nowCustomerPF();
         assertNotNull(customer);
         assertEquals("Claudio Carigé", customer.getClientName());
@@ -26,7 +26,7 @@ class CustomerTest {
     @Test
     @DisplayName("You must create customer legal person customer")
     void youMustCreateCustomerLegalPersonCustomer() {
-        LegalPersonalCustomer customer = oneCustomer().withCnpj("23.456.789/0001-10").withClientName("CopyImagem").nowCustomerPJ();
+        LegalPersonalCustomer customer = oneLegalPersonalCustomer().withCnpj("23.456.789/0001-10").withClientName("CopyImagem").nowCustomerPJ();
         assertNotNull(customer);
         assertEquals("CopyImagem", customer.getClientName());
         assertEquals("23.456.789/0001-10", customer.getCnpj());
