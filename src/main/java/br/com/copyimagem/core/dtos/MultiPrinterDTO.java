96ea7cbb117ee04cf8cf2dde63cbc04ac8c41e9a
Claudio Carigé <ccarige@gmail.com>
2024-05-15 16:44:00 -0300

    [ addition ] : Created MultiPrinterDTO representation class

---
+++ src/main/java/br/com/copyimagem/core/dtos/MultiPrinterDTO.java
@@ -0,0 +1,20 @@
+package br.com.copyimagem.core.dtos;
+
+import br.com.copyimagem.core.domain.enums.MachineStatus;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MultiPrinterDTO {
+
+    private Integer id;
+    private String brand;
+    private String model;
+    private String serialNumber;
+    private Double machineValue;
+    private MachineStatus machineStatus;
+    private Integer impressionCounter;
+}
