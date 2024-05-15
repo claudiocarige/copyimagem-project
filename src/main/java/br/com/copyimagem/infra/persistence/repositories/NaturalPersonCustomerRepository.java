654c0c3ae18819b8fff54c288631f10bb25f2449
Claudio Carigé <ccarige@gmail.com>
2024-05-14 21:21:58 -0300

    [ addition ] : Inserted existsNaturalPersonCustomerByCpf method

--- src/main/java/br/com/copyimagem/infra/persistence/repositories/NaturalPersonCustomerRepository.java
+++ src/main/java/br/com/copyimagem/infra/persistence/repositories/NaturalPersonCustomerRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface NaturalPersonCustomerRepository extends JpaRepository<NaturalPersonCustomer, Long>{
-    Optional<NaturalPersonCustomer> findByPrimaryEmail(String primaryEmail);
-
     Optional<NaturalPersonCustomer> findByCpf(String cpf);
+
+    Boolean existsNaturalPersonCustomerByCpf(String cpf);
 }
