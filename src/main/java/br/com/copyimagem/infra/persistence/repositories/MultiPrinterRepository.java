f92d866ce290a20fbc084005c4840f7093b5e9dc
Claudio Carigé <ccarige@gmail.com>
2024-05-15 23:25:40 -0300

    [ cleanup ] : MultiPrinterR  code cleaning

--- src/main/java/br/com/copyimagem/infra/persistence/repositories/MultiPrinterRepository.java
+++ src/main/java/br/com/copyimagem/infra/persistence/repositories/MultiPrinterRepository.java
@@ -3,7 +3,6 @@ package br.com.copyimagem.infra.persistence.repositories;
 import br.com.copyimagem.core.domain.entities.MultiPrinter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface MultiPrinterRepository extends JpaRepository<MultiPrinter, Integer>{
 }
