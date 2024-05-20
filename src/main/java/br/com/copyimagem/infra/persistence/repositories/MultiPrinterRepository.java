97b7eae40b4e8426ae9a9a14f67fcc4b0d0c7507
Claudio Carigé <ccarige@gmail.com>
2024-05-20 13:14:40 -0300

    [ addition ] : Created updateMachineStatusById method in MultiPrinterRepository

--- src/main/java/br/com/copyimagem/infra/persistence/repositories/MultiPrinterRepository.java
+++ src/main/java/br/com/copyimagem/infra/persistence/repositories/MultiPrinterRepository.java
@@ -1,9 +1,18 @@
 package br.com.copyimagem.infra.persistence.repositories;
 
 import br.com.copyimagem.core.domain.entities.MultiPrinter;
+import br.com.copyimagem.core.domain.enums.MachineStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface MultiPrinterRepository extends JpaRepository<MultiPrinter, Integer>{
     boolean existsBySerialNumber(String serialNumber);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE MultiPrinter mp SET mp.machineStatus = :status WHERE mp.id = :id")
+    MultiPrinter updateMachineStatusById(Integer id, MachineStatus status);
 }
