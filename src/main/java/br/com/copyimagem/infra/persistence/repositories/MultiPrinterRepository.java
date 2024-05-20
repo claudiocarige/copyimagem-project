69196259f3da7d8836dd3e3f67f4f0a5ada92a47
Claudio Carigé <ccarige@gmail.com>
2024-05-20 13:43:43 -0300

    [ addition ] : Created updateImpressionCounterById in MultiPrinterRepository

--- src/main/java/br/com/copyimagem/infra/persistence/repositories/MultiPrinterRepository.java
+++ src/main/java/br/com/copyimagem/infra/persistence/repositories/MultiPrinterRepository.java
@@ -5,6 +5,7 @@ import br.com.copyimagem.core.domain.enums.MachineStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -14,5 +15,13 @@ public interface MultiPrinterRepository extends JpaRepository<MultiPrinter, Inte
     @Transactional
     @Modifying
     @Query("UPDATE MultiPrinter mp SET mp.machineStatus = :status WHERE mp.id = :id")
-    MultiPrinter updateMachineStatusById(Integer id, MachineStatus status);
+    MultiPrinter updateMachineStatusById(@Param(value = "id") Integer id, @Param(value="status") MachineStatus status);
+
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE MultiPrinter mp SET " +
+            "mp.impressionCounter = :counter WHERE mp.id = :id AND :counter > mp.impressionCounter")
+    MultiPrinter updateImpressionCounterById(
+                         @Param(value = "id") Integer id, @Param(value = "counter") Integer counter);
 }
