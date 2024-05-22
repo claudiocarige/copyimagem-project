9c696fc8e29ece1a7b4fba2846e2feb921242754
Claudio Carigé <ccarige@gmail.com>
2024-05-22 19:17:23 -0300

    [ refactor ] : Deleted class constructor

--- src/main/java/br/com/copyimagem/core/domain/entities/MonthlyPayment.java
+++ src/main/java/br/com/copyimagem/core/domain/entities/MonthlyPayment.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 @Entity
 public class MonthlyPayment implements Serializable {
@@ -30,7 +31,6 @@ public class MonthlyPayment implements Serializable {
     private Integer quantityPrints;
     private Double excessValuePrints;
     private Double amountPrinter;
-    //new attributes
     private String invoiceNumber;
     private String ticketNumber;
     private Integer printingFranchise;
@@ -47,29 +47,4 @@ public class MonthlyPayment implements Serializable {
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
-
-    public MonthlyPayment(Long id, Integer monthPayment, Integer yearPayment,
-                          Double monthlyAmount, Integer impressionsCounter,
-                          Integer quantityPrints, Double excessValuePrints,
-                          Double amountPrinter, String invoiceNumber,
-                          String ticketNumber, Integer printingFranchise,
-                          LocalDate expirationDate, LocalDate paymentDate,
-                          PaymentStatus paymentStatus, Customer customer) {
-        this.id = id;
-        this.monthPayment = monthPayment;
-        this.yearPayment = yearPayment;
-        this.monthlyAmount = monthlyAmount;
-        this.impressionsCounter = impressionsCounter;
-        this.quantityPrints = quantityPrints;
-        this.excessValuePrints = excessValuePrints;
-        this.amountPrinter = amountPrinter;
-        this.invoiceNumber = invoiceNumber;
-        this.ticketNumber = ticketNumber;
-        this.printingFranchise = printingFranchise;
-        this.expirationDate = expirationDate;
-        this.paymentDate = paymentDate;
-        this.paymentStatus = paymentStatus;
-        this.customer = customer;
-    }
-
 }
