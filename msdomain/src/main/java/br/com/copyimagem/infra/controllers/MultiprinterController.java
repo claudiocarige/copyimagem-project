3c1648691e531e550f597e00c43f4afcf5640d6c
Claudio Carigé <ccarige@gmail.com>
2024-06-19 22:23:28 -0300

    [ addition ] : Created setUpClientOnAMultiPrinter method in MultiprinterController

--- msdomain/src/main/java/br/com/copyimagem/infra/controllers/MultiprinterController.java
+++ msdomain/src/main/java/br/com/copyimagem/infra/controllers/MultiprinterController.java
@@ -9,34 +9,48 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping( "/api/v1/multiprinter" )
+@RequestMapping( "/api/v1/multi-printer" )
 public class MultiprinterController {
+
 
     private final MultiPrinterService multiPrinterService;
 
 
     public MultiprinterController( MultiPrinterService multiPrinterService ) {
+
         this.multiPrinterService = multiPrinterService;
     }
 
     @GetMapping
     public ResponseEntity< List< MultiPrinterDTO > > findAllMultiPrinters() {
+
         return ResponseEntity.ok( multiPrinterService.findAllMultiPrinters() );
     }
 
     @GetMapping( "/{id}" )
-    public ResponseEntity< MultiPrinterDTO > findMultiPrinterById(@PathVariable Integer id ) {
+    public ResponseEntity< MultiPrinterDTO > findMultiPrinterById( @PathVariable Integer id ) {
+
         return ResponseEntity.ok( multiPrinterService.findMultiPrinterById( id ) );
     }
 
     @GetMapping( "/customer/{customerId}" )
-    public ResponseEntity< List< MultiPrinterDTO > > findAllMultiPrintersByCustomerId(@PathVariable Long customerId ) {
+    public ResponseEntity< List< MultiPrinterDTO > > findAllMultiPrintersByCustomerId( @PathVariable Long customerId ) {
+
         return ResponseEntity.ok( multiPrinterService.findAllMultiPrintersByCustomerId( customerId ) );
     }
 
-    @PostMapping( "/save" )
-    public ResponseEntity< MultiPrinterDTO > saveMultiPrinter(@RequestBody MultiPrinterDTO multiPrinterDTO ) {
+    @PostMapping( value = "/save")
+    public ResponseEntity< MultiPrinterDTO > saveMultiPrinter( @RequestBody MultiPrinterDTO multiPrinterDTO ) {
+
         return ResponseEntity.ok( multiPrinterService.saveMultiPrinter( multiPrinterDTO ) );
     }
+
+    @PatchMapping( "/set-customer" )
+    public ResponseEntity< MultiPrinterDTO > setUpClientOnAMultiPrinter( @RequestParam Integer id,
+                                                                         @RequestParam Long customerId ) {
+
+        return ResponseEntity.ok( multiPrinterService.setUpClientOnAMultiPrinter( id, customerId ) );
+    }
+
 
 }
